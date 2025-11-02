aa0ed68757b3b70dbd43211552b639759ee90b2b
SRYVarder <smallreezy3@gmail.com>
2025-11-02 20:25:18 +0200

    Change property_list to return JSON response
    Updated property_list view to return JSON response with property details instead of rendering an HTML template.

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .utils import get_all_properties
 
+
 @cache_page(60 * 15)
 def property_list(request):
+    """
+    View to list all properties.
+    The response is cached for 15 minutes in Redis.
+    """
     properties = get_all_properties()
-    return render(request, "properties/property_list.html", {"properties": properties})
+    data = [
+        {
+            "id": prop.id,
+            "title": prop.title,
+            "description": prop.description,
+            "price": float(prop.price),
+            "location": prop.location,
+            "created_at": prop.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        for prop in properties
+    ]
+    return JsonResponse({"data": data})
